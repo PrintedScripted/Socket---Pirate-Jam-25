5a4c79fd0d64b84202192ed5fbcd150a97441bc0
PrintedScripted <remarkablescholar@gmail.com>
2025-01-22 07:47:26 -0700

    Update Dasher.gd

--- socket---pirate-jam-2025/Scripts/Enemy/Dasher.gd
+++ socket---pirate-jam-2025/Scripts/Enemy/Dasher.gd
@@ -1,1 +1,13 @@
-extends Enemy
+class_name Dasher extends Enemy
+
+@export var circle_distance: int = 300
+
+func _physics_process(delta: float) -> void:
+	var distance = position.distance_to(Vector2(0,0))
+	velocity = position.direction_to(Vector2(0,0)) * speed * delta
+	if distance > circle_distance:
+		move_and_slide()
+	else:
+		pass
+	look_at(Vector2(0,0))
+	rotation_degrees += 90
